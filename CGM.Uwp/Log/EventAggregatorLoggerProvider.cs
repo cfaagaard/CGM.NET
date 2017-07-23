a9d3e75d7af14d8abac994fa921fd5b8d210b474
Christian Aagaard <cfa@innovait.dk>
2017-07-23 16:10:13 +0200

    adding ignored files???

---
+++ CGM.Uwp/Log/EventAggregatorLoggerProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace CGM.Uwp.Log
+{
+    public class EventAggregatorLoggerProvider : ILoggerProvider
+    {
+        private Func<string, LogLevel, bool> _filter;
+
+        public EventAggregatorLoggerProvider(Func<string, LogLevel, bool> filter)
+        {
+            _filter = filter;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new EventAggregatorLogger(categoryName, _filter);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
