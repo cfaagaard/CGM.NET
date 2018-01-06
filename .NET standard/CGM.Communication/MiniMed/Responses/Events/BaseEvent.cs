b990dbe27ad4cf44c2eb4ca87fdffc1c0020280c
cfaagaard <cfa@innovait.dk>
2018-01-06 20:49:43 +0100

    fixed bug in sensor_event

--- .NET standard/CGM.Communication/MiniMed/Responses/Events/BaseEvent.cs
+++ .NET standard/CGM.Communication/MiniMed/Responses/Events/BaseEvent.cs
@@ -67,19 +67,28 @@ namespace CGM.Communication.MiniMed.Responses.Events
             this.AllBytes = bytes;
             this.AllBytesE = bytes.Reverse().ToArray();
             this.BytesAsString = BitConverter.ToString(AllBytes);
-            if (this.Message.GetType().Equals(typeof(SENSOR_GLUCOSE_READINGS_EXTENDED_Event)))
+            if (this.Message!=null && this.Message.GetType().Equals(typeof(SENSOR_GLUCOSE_READINGS_EXTENDED_Event)))
             {
-                var reading = (SENSOR_GLUCOSE_READINGS_EXTENDED_Event)this.Message;
-                for (int i = 0; i < reading.Details.Count; i++)
+                try
                 {
-                    if (reading.Timestamp.HasValue)
+                    var reading = (SENSOR_GLUCOSE_READINGS_EXTENDED_Event)this.Message;
+                    for (int i = 0; i < reading.Details.Count; i++)
                     {
-                        var read = reading.Details[i];
-                        var readingRtc = this.Rtc - (i * reading.MinutesBetweenReadings * 60);
-                        read.Timestamp = DateTimeExtension.GetDateTime(readingRtc, this.Offset);
-                        read.PredictedSg = reading.PredictedSg;
+                        if (reading.Timestamp.HasValue)
+                        {
+                            var read = reading.Details[i];
+                            var readingRtc = this.Rtc - (i * reading.MinutesBetweenReadings * 60);
+                            read.Timestamp = DateTimeExtension.GetDateTime(readingRtc, this.Offset);
+                            read.PredictedSg = reading.PredictedSg;
+                        }
                     }
                 }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+    
             }
         }
 
